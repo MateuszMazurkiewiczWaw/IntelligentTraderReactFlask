1d91ec6b61c4249ab1fa7b6d97b85606703bef64
MateuszMazurkiewiczWaw <mateusz.mazurkiewiczwaw@gmail.com>
2020-06-15 07:35:52 +0200

    5th commit

---
+++ flask_backend/static/react/precache-manifest.aa4d957a2d96fea01e3985bb7e68db20.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e58b5e1363434f66eaaf",
+    "url": "/static/react/css/main.b357ed75.chunk.css"
+  },
+  {
+    "revision": "e58b5e1363434f66eaaf",
+    "url": "/static/react/js/main.e58b5e13.chunk.js"
+  },
+  {
+    "revision": "c2b7f5bba891fc6da613",
+    "url": "/static/react/js/runtime~main.c2b7f5bb.js"
+  },
+  {
+    "revision": "3aefdf834f8b4c44b120",
+    "url": "/static/react/css/2.c6250e3a.chunk.css"
+  },
+  {
+    "revision": "3aefdf834f8b4c44b120",
+    "url": "/static/react/js/2.3aefdf83.chunk.js"
+  },
+  {
+    "revision": "8cb599582beb7172634bc639287c9694",
+    "url": "/static/react/media/logo_light.8cb59958.svg"
+  },
+  {
+    "revision": "ce171536f6bec8cac938d536d4179f06",
+    "url": "/static/react/media/logo_dark.ce171536.svg"
+  },
+  {
+    "revision": "5883aae9f1e587c53b497e7d7d5c6fb4",
+    "url": "/static/react/../../templates/index.html"
+  }
+]);
